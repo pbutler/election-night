41e1cb1cc23f27793e166f5b35767c1b4dd25fd1
Patrick Butler <pbutler@killertux.org>
2012-11-06 18:11:56 -0500

    fixes

--- get_results.sh
+++ get_results.sh
@@ -5,12 +5,11 @@ LAST=`md5sum results.json | awk '{print $1}'`
 while [ -t ]; do
 	curl -s -S "http://data.cnn.com/ELECTION/2012/full/P.full.json" > results.json
 	NOW=`md5sum results.json | awk '{print $1}'`
-	NOW=1
 	if [ $NOW != $LAST ]; then
 		T=`date`
 		git commit -a -m"Election data update at ${T}"
 		git push
-
+		echo "done"
 	fi
 	LAST=$NOW
 	sleep 60
